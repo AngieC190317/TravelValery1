6d04c48fb365e31e97b003467e071f43655be05a
AngieC190317 <160764949+AngieC190317@users.noreply.github.com>
2024-11-22 18:34:13 -0500

    credenciales

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -10,7 +10,9 @@ export class LoginPage {
   username: string = '';
   password: string = '';
   passwordType: string = 'password'; // Estado inicial de la contraseña oculta
-  constructor(private router: Router) {}
+
+  constructor(private authService: AuthService, private router: Router) {}
+
  // Función para alternar la visibilidad de la contraseña
  togglePassword() {
   this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
@@ -20,15 +22,12 @@ goToRegister() {
   this.router.navigate(['/register']); // Cambia "/register" según la ruta de tu página de registro
 }
 
-
-  login() {
-    if (this.username && this.password) {
-      // Aquí iría la lógica para autenticar al usuario, por ejemplo, con una API.
-      alert(`Iniciando sesión con ${this.username}`);
-      // Si la autenticación es exitosa, redirige al usuario
-      this.router.navigate(['/tabs']);
-    } else {
-      alert('Por favor, ingresa tus datos.');
-    }
+login() {
+  if (this.authService.login(this.username, this.password)) {
+    // Si las credenciales son correctas, navegar a la página principal o dashboard
+    this.router.navigate(['/tabs']);
+  } else {
+    alert('Credenciales incorrectas');
   }
+}
 }
